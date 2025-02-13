05bda2e6ebc5ff3a013c60e5a512388e17978833
Kanad <codewizardkanad@gmail.com>
2025-02-13 12:31:44 +0530

    Made the reusable user button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,9 @@
-"use client";
-
-import { Button } from "@/components/ui/button";
-import { useCurrentUser } from "@/features/auth/api/use-current-user";
-import { useLogout } from "@/features/auth/api/use-logout";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { UserButton } from "@/features/auth/components/user-button";
 
 export default function Home() {
-  const router = useRouter();
-  const { data, isLoading } = useCurrentUser();
-  const { mutate } = useLogout();
-
-  useEffect(() => {
-    if (!data && !isLoading) {
-      router.push("/sign-in");
-    }
-  }, [data]);
-
-  console.log("Data i am getting now: ", data);
-
   return (
-    <div className="flex gap-2">
-      Only visible to authorized user.
-      <Button onClick={() => mutate()}>Logout</Button>
+    <div>
+      <UserButton />
     </div>
   );
 }
