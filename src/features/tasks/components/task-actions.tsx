2f9181a9ad611221760c7f6a382b337252b2c5f9
Kanad <codewizardkanad@gmail.com>
2025-02-16 12:19:26 +0530

    Added task deletion functionality for all members under a project

--- src/features/tasks/components/task-actions.tsx
+++ src/features/tasks/components/task-actions.tsx
@@ -4,7 +4,9 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { ExternalLinkIcon } from "lucide-react";
+import { ExternalLinkIcon, PencilIcon, TrashIcon } from "lucide-react";
+import { useDeleteTask } from "../api/use-delete-task";
+import { useConfirm } from "@/hooks/user-confirmation-modal";
 
 interface TaskActionsProps {
   id: string;
@@ -13,18 +15,47 @@ interface TaskActionsProps {
 }
 
 export const TaskActions = ({ children, id, projectId }: TaskActionsProps) => {
+  const { mutate: deleteTask, isPending: isTaskDeletionPending } =
+    useDeleteTask();
+
+  const [ConfirmDeleteTaskDialog, confirmDeleteTask] = useConfirm(
+    "Delete Task",
+    "This action can't be undone!",
+    "destructive"
+  );
+
+  const onTaskDelete = async () => {
+    const ok = await confirmDeleteTask();
+    if (!ok) return;
+
+    deleteTask({ param: { taskId: id } });
+  };
+
   return (
     <div className="flex justify-end">
+      <ConfirmDeleteTaskDialog />
       <DropdownMenu modal={false}>
         <DropdownMenuTrigger asChild>{children}</DropdownMenuTrigger>
         <DropdownMenuContent className="w-48" align="end">
-          <DropdownMenuItem
-            onClick={() => {}}
-            disabled={false}
-            className="font-medium p-[10px]"
-          >
+          <DropdownMenuItem onClick={() => {}} className="font-medium p-[10px]">
             <ExternalLinkIcon className="size-4 mr-2 stroke-2" />
             Task Details
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => {}} className="font-medium p-[10px]">
+            <ExternalLinkIcon className="size-4 mr-2 stroke-2" />
+            Open Project
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => {}} className="font-medium p-[10px]">
+            <PencilIcon className="size-4 mr-2 stroke-2" />
+            Edit Task
+          </DropdownMenuItem>
+          <DropdownMenuItem
+            onClick={onTaskDelete}
+            disabled={isTaskDeletionPending}
+            className="text-rose-600 focus:text-rose-600 font-medium p-[10px]"
+          >
+            <TrashIcon className="size-4 mr-2 stroke-2" />
+            Delete Task
           </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
