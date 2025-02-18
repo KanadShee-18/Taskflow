41e9ec7125a1c66889257f415ee2da9c05a8a34b
Kanad <codewizardkanad@gmail.com>
2025-02-18 23:21:43 +0530

    Customized the landing page a bit

--- src/features/auth/components/auth-landing.tsx
+++ src/features/auth/components/auth-landing.tsx
@@ -4,18 +4,47 @@ import Image from "next/image";
 
 export const AuthClientPage = () => {
   return (
-    <div className="absolute w-full h-full">
+    <div className="absolute w-full h-full pt-14">
+      <div className="w-1/2 aspect-square inset-0 m-auto  fixed z-[3] opacity-50 bg-gradient-to-br from-slate-800 to-indigo-950 blur-[50px] lg:blur-[150px]" />
+
       <Image
-        src="/Images/project-kanban.png"
+        src="/Images/auth/kanban.png"
         alt="Project 1"
-        width={500}
+        width={600}
         height={300}
-        className="w-11/12 mx-auto z-[2] m-4 pt-5 -rotate-1 opacity-60 blur-[0.5px]"
+        className=" mr-auto z-[2] m-4 pt-5 opacity-90 "
         quality={100}
         unoptimized
       />
-
-      <div className="w-full h-full fixed top-0 left-0 opacity-50 bg-gradient-to-tr from-indigo-950 to-transparent" />
+      <Image
+        src="/Images/auth/table.png"
+        alt="Project 1"
+        width={700}
+        height={300}
+        className="absolute md:-right-16 top-14 z-[2] m-4 pt-5 opacity-95 "
+        quality={100}
+        unoptimized
+      />
+      <div className="w-full flex justify-between flex-row">
+        <Image
+          src="/Images/auth/home.png"
+          alt="Project 1"
+          width={400}
+          height={200}
+          className="mr-auto w-auto max-w-[600px] z-[2] m-4 pt-5 "
+          quality={100}
+          unoptimized
+        />
+        <Image
+          src="/Images/auth/member.png"
+          alt="Project 1"
+          width={400}
+          height={300}
+          className="z-[2] m-4 pt-5 md:w-[500px]"
+          quality={100}
+          unoptimized
+        />
+      </div>
     </div>
   );
 };
